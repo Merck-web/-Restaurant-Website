9b09d1bcbcc27ea4ef6014fc14a5135cdcc3e058
Your Name <you@example.com>
2021-10-27 18:43:08 +0500

    75%

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// toggle nav
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav(){
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
+}
+// close nav
+document.addEventListener("click", function(event){
+    if(event.target.closest(".nav-item")){
+        toggleNav();
+    }
+})
+//sticky
+window.addEventListener("scroll", function(){
+    if(this.pageYOffset > 60){
+        document.querySelector(".header").classList.add("sticky");
+    }else{
+        document.querySelector(".header").classList.remove("sticky");
+    }
+})
+//menu tabs
+const menuTabs = document.querySelector(".menu-tabs");
+menuTabs.addEventListener("click", function(event){
+    if(event.target.classList.contains("menu-tab-item") && !event.target.classList.contains("active")){
+        const target = event.target.getAttribute("data-target");
+        menuTabs.querySelector(".active").classList.remove("active");
+        event.target.classList.add("active")
+
+        //content toggle
+        const menuSection = document.querySelector(".menu-section");
+        menuSection.querySelector(".menu-tab-content.active").classList.remove("active");
+        menuSection.querySelector(target).classList.add("active");
+    }
+})
